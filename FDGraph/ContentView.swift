883c19644415b1efef49cbefbcd88ca31b168324
Hevi <hevi-public@gmail.com>
2020-08-14 20:32:13 +0100

    Initial Commit

---
+++ FDGraph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FDGraph
+//
+//  Created by Hevi on 14/08/2020.
+//  Copyright © 2020 Hevi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
