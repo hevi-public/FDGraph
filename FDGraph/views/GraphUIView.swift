e69c6ff774db92501b055c769a20e373d6b291e4
Hevi <hevi-public@gmail.com>
2020-08-17 19:45:13 +0100

    refactor

--- FDGraph/views/GraphUIView.swift
+++ FDGraph/views/GraphUIView.swift
@@ -13,7 +13,48 @@ struct GraphUIView: UIViewControllerRepresentable {
     typealias UIViewControllerType = GraphController
     
     func makeUIViewController(context: UIViewControllerRepresentableContext<GraphUIView>) -> GraphController {
-        GraphController()
+        let graphController = GraphController()
+        
+        let radius = CGFloat(10)
+        
+        let nodes = [
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius * 1.1)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius * 1.4)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius * 1.2)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius * 1.3)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius * 1.1)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius * 1.5)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius * 1.6)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius * 1.5)),
+            Node(view: Circle.createCircle(radius: radius)),
+            Node(view: Circle.createCircle(radius: radius))
+        ]
+        
+        graphController.add(nodes: nodes)
+        graphController.add(edges: [])
+        
+        return graphController
     }
     
     func updateUIViewController(_ uiViewController: GraphController, context: UIViewControllerRepresentableContext<GraphUIView>) {
