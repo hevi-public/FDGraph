d1216d24a356cefcb40e912a8d072abf46c513c2
Hevi <hevi-public@gmail.com>
2020-08-17 20:09:07 +0100

    cleanup

--- FDGraph/views/GraphView.swift
+++ FDGraph/views/GraphView.swift
@@ -11,39 +11,16 @@ import UIKit
 
 class GraphView: UIView {
     
-//    private var tickCallback: () -> ()
-//    private var tapCircleCallback: (Node) -> ()
-    
     private lazy var graph: GraphEngine = {
         let graphEngine = GraphEngine(containerView: self)
         return graphEngine
     }()
     
-    var circles: [Circle] = []
-    
-    let edgeLayer: EdgeLayer = EdgeLayer()
-    
-    init(width: CGFloat, height: CGFloat)
-//    ,
-//         tickCallback: @escaping () -> (),
-//         tapCircleCallback: @escaping (Node) -> ())
-    {
+    init(width: CGFloat, height: CGFloat) {
         
         let frame = CGRect(x: 0, y: 0, width: width, height: height)
-        
-        
-        
-//        self.tickCallback = tickCallback
-//        self.tapCircleCallback = tapCircleCallback
-        
     
         super.init(frame: frame)
-        
-        
-        self.layer.insertSublayer(edgeLayer, at: 0)
-        
-        
-        
         
         
     }
@@ -51,16 +28,6 @@ class GraphView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
- 
-
-//    @objc func handleTap(sender: UITapGestureRecognizer) {
-//        guard let node = (sender.view as? Circle)?.node else { return }
-//
-//        self.tapCircleCallback(node)
-//    }
-    
-    
     
 }
 
